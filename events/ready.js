11f070e42c7f19134137d38de9a6a60e0279f61d
Benjamin Gaertner <benjaming@outlook.fr>
2023-05-23 19:21:24 +0200

    admin command

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,9 @@ module.exports = {
 	execute(bot) {
 		console.log(`${color.cyan}[Ready]		${color.blue}${bot.user.tag} ${color.green}online.${color.stop}`);
 
-		//client.application.commands.set([])
+		bot.application.commands.set([]);
+		bot.application.adminCommands.set([]);
+		console.log(`${color.cyan}[Ready]		${color.red}Emptyed commands and adminCommands tables.${color.stop}`);
 		bot.user.setPresence({
 			status: 'online',
 		})
