eb481e55ac247d0b88b221f0e6bef0a40fdcde8a
Benjamin Gaertner <benjaming@outlook.fr>
2023-05-23 19:11:32 +0200

    admin command

--- main.js
+++ main.js
@@ -4,57 +4,60 @@ const fs = require('node:fs');
 const color = require("./resources/color_codes");
 
 const intents = new Discord.IntentsBitField(3276799);
-const bot = new Discord.Client({intents});
+const bot = new Discord.Client({ intents });
 
 bot.commands = new Discord.Collection();
 bot.commandArray = [];
-fs.readdirSync("./commands").filter(files =>
-	files.endsWith(".js")).forEach(file => {
+const reglarCommandFiles = fs.readdirSync("./commands").filter(files => files.endsWith(".js"))
+if (reglarCommandFiles.length > 0) {
+	reglarCommandFiles.forEach(file => {
 		const command = require(`./commands/${file}`);
 		bot.commands.set(command.data.name, command);
 		bot.commandArray.push(command.data.toJSON());
 		console.log(`${color.cyan}[Main]	${color.yellow}${file.slice(0, file.length - 3)} ${color.white} is operational.${color.cyan}`);
 	}
-);
+	);
 
-const rest = new Discord.REST({ version: '10' }).setToken(config.token);
-(async () => {
-	try {
-		console.log(`${color.cyan}[Main]	${color.yellow}Trying to update application commands.${color.stop}`);
-		const data = await rest.put(
-			Discord.Routes.applicationCommands( config.clientId ),
-			{ body: bot.commandArray },
-		);
-		console.log(`${color.cyan}[Main]	${color.yellow}Successfully updated ${data.length} application commands.${color.stop}`);
-	} catch (error) {
-		console.error(error);
-	}
-})();
+	const rest = new Discord.REST({ version: '10' }).setToken(config.token);
+	(async () => {
+		try {
+			console.log(`${color.cyan}[Main]	${color.yellow}Trying to update application commands.${color.stop}`);
+			const data = await rest.put(
+				Discord.Routes.applicationCommands(config.clientId),
+				{ body: bot.commandArray },
+			);
+			console.log(`${color.cyan}[Main]	${color.yellow}Successfully updated ${data.length} application commands.${color.stop}`);
+		} catch (error) {
+			console.error(error);
+		}
+	})();
+}
 
 
 bot.adminCommands = new Discord.Collection();
 bot.adminCommandArray = [];
-fs.readdirSync("./admin").filter(files =>
-	files.endsWith(".js")).forEach(file => {
+const adminCommandFiles = fs.readdirSync("./admin").filter(files => files.endsWith(".js"));
+if (adminCommandFiles.length > 0) {
+	adminCommandFiles.forEach(file => {
 		const command = require(`./admin/${file}`);
 		bot.adminCommands.set(command.data.name, command);
 		bot.adminCommandArray.push(command.data.toJSON());
 		console.log(`${color.red}[Admin] ${color.yellow}${file.slice(0, file.length - 3)} ${color.white} is operational.${color.cyan}`);
 	}
-);
-(async () => {
-	try {
-		console.log(`${color.red}[Admin]	${color.yellow}Trying to update admin commands.${color.stop}`);
-		const data = await rest.put(
-			Discord.Routes.applicationGuildCommands( config.clientId, config.adminGuildId ),
-			{ body: bot.adminCommandArray },
-		);
-		console.log(`${color.red}[Admin]	${color.yellow}Successfully updated ${data.length} admin commands.${color.stop}`);
-	} catch (error) {
-		console.error(error);
-	}
-})();
-
+	);
+	(async () => {
+		try {
+			console.log(`${color.red}[Admin]	${color.yellow}Trying to update admin commands.${color.stop}`);
+			const data = await rest.put(
+				Discord.Routes.applicationGuildCommands(config.clientId, config.adminGuildId),
+				{ body: bot.adminCommandArray },
+			);
+			console.log(`${color.red}[Admin]	${color.yellow}Successfully updated ${data.length} admin commands.${color.stop}`);
+		} catch (error) {
+			console.error(error);
+		}
+	})();
+}
 
 fs.readdirSync("./events").filter(files =>
 	files.endsWith(".js")).forEach(file => {
@@ -64,6 +67,6 @@ fs.readdirSync("./events").filter(files =>
 		else
 			bot.on(event.name, (...args) => event.execute(...args));
 	}
-);
+	);
 
 bot.login(config.token);
