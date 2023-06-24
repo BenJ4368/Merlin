44bd17208852d0de06be49d78d3ec13bd8c9d1a9
Benjamin Gaertner <benjaming@outlook.fr>
2023-06-24 14:43:43 +0200

    readme updates

--- events/ready.js
+++ events/ready.js
@@ -5,13 +5,13 @@ module.exports = {
 	name: Discord.Events.ClientReady,
 	once: true,
 	execute(bot) {
-		console.log(`${color.cyan}[Ready]		${color.blue}${bot.user.tag} ${color.green}online.${color.stop}`);
+		console.log(`${color.cyan}[Ready]		${color.blue}${bot.user.tag} ${color.green}online${color.stop}.`);
 
 		bot.user.setPresence({
 			status: 'online',
 		})
 
 		const guildsNbr = bot.guilds.cache.size;
-		console.log(`${color.cyan}[Ready]		${color.blue}${bot.user.tag} ${color.stop}is present in ${color.green}${guildsNbr}.${color.stop}`);
+		console.log(`${color.cyan}[Ready]		${color.blue}${bot.user.tag} ${color.stop}is present in ${color.green}${guildsNbr} servers${color.stop}.`);
 	},
 };
