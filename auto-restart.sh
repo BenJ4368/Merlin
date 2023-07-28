347ffeda0652b4584bce8d289b3b58955b69bece
Benjamin Gaertner <benjaming@outlook.fr>
2023-07-29 01:00:59 +0200

    durqtion updte

--- auto-restart.sh
+++ auto-restart.sh
@@ -2,9 +2,10 @@
 
 RED='\033[0;31m'
 STOP='\033[0m'
+
 # Auto restarts the bot when it crashes
 while true; do
 	git pull
 	node ./main.js
-	echo -e "${RED}MERLIN - Auto-restart ${NC} \n"
+	echo -e "${RED}	MERLIN - Auto-restart ${STOP} \n"
 done
