19f772985416b3d720f924d4c7e2ee14a2d515e6
Benjamin Gaertner <benjaming@outlook.fr>
2023-08-24 10:00:19 +0200

    Merlin update

--- events/ready.js
+++ events/ready.js
@@ -12,7 +12,7 @@ module.exports = {
 		bot.user.setActivity({	// Sets the activity
 			name: "BenJ's best action",	// Name of the game/stream/music etc
 			type: Discord.ActivityType.Streaming,	// Type of activity (streaming, playing, listening to, competing etc)
-			url: "https://www.youtube.com/watch?v=4Gw5aui7efo"	// 'url' property only works with streaming type
+			url: "https://www.youtube.com/@BenJ-fe1mi/videos"	// 'url' property only works with streaming type
 		})
 
 		const guildsNbr = bot.guilds.cache.size;  // Stores the numbers of server the bot is in
