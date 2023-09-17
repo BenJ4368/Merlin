8b6cc16bbe78a3415a18758cc4b2c235c3b9853f
Benjamin Gaertner <benjaming@outlook.fr>
2023-09-17 14:04:35 +0200

    smite challenge comeback

--- admin/_dlskins.js
+++ admin/_dlskins.js
@@ -17,7 +17,7 @@ async function dlskins() {
 		// 		output: process.stdout,
 		// 	});
 
-		// 	prompt.question(`${color.cyan}[dlskins] ${color.lime}Starting position ? (0 - 125)${color.stop}`,
+		// 	prompt.question(`${color.cya}[dlskins] ${color.lime}Starting position ? (0 - 125)${color.stop}`,
 		// 		(promptAnswer) => {
 		// 			prompt.close();
 		// 			if (promptAnswer) {
@@ -42,7 +42,7 @@ async function dlskins() {
 			// 		output: process.stdout,
 			// 	});
 
-			// 	prompt.question(`${color.cyan}[dlskins] ${color.lime}Start downloading ${god.Name}'s skins? (${godNumber})${color.stop}`,
+			// 	prompt.question(`${color.cya}[dlskins] ${color.lime}Start downloading ${god.Name}'s skins? (${godNumber})${color.stop}`,
 			// 		(promptAnswer) => {
 			// 			prompt.close();
 			// 			if (promptAnswer.toLocaleLowerCase() === 'y') {
@@ -74,36 +74,36 @@ async function dlskins() {
 								.then(async (response) => {
 									const dest = fs.createWriteStream(savePath);
 									response.body.pipe(dest);
-									console.log(`${color.cyan}[dlskins] ${color.white}${fileName} ${color.green}was successfully downloaded.${color.stop}`);
+									console.log(`${color.cya}[dlskins] ${color.whi}${fileName} ${color.grn}was successfully downloaded.${color.stop}`);
 								})
 								.catch((error) => {
-									console.log(`${color.cyan}[dlskins] ${color.white}${fileName} ${color.red}downloading error: ${color.yellow}${error.message}${color.stop}`);
+									console.log(`${color.cya}[dlskins] ${color.whi}${fileName} ${color.red}downloading error: ${color.yellow}${error.message}${color.stop}`);
 								});
 						}
 						// else {
 						// 	try {
 						// 		const image = await Jimp.read(savePath);
 						// 		if (image.getWidth() >= 250 && image.getHeight() >= 250) {
-						// 			console.log(`${color.cyan}[dlskins] ${color.white}${fileName} ${color.green}is valid.${color.stop}`);
+						// 			console.log(`${color.cya}[dlskins] ${color.whi}${fileName} ${color.grn}is valid.${color.stop}`);
 						// 		} else {
-						// 			console.log(`${color.cyan}[dlskins] ${color.white}${fileName} ${color.red}is not valid. ${color.yellow}Deleting...${color.stop}`);
+						// 			console.log(`${color.cya}[dlskins] ${color.whi}${fileName} ${color.red}is not valid. ${color.yellow}Deleting...${color.stop}`);
 						// 			fs.unlinkSync(savePath);
-						// 			console.log(`${color.cyan}[dlskins] ${color.red}${fileName} deleted.${color.stop}`);
+						// 			console.log(`${color.cya}[dlskins] ${color.red}${fileName} deleted.${color.stop}`);
 						// 		}
 						// 	} catch (error) {
-						// 		console.log(`${color.cyan}[dlskins] ${color.white}${fileName} ${color.red}is not valid :\n ${color.yellow}${error}${color.stop}`);
+						// 		console.log(`${color.cya}[dlskins] ${color.whi}${fileName} ${color.red}is not valid :\n ${color.yellow}${error}${color.stop}`);
 						// 		fs.unlinkSync(savePath);
-						// 		console.log(`${color.cyan}[dlskins] ${color.red}${fileName} deleted.${color.stop}`);
+						// 		console.log(`${color.cya}[dlskins] ${color.red}${fileName} deleted.${color.stop}`);
 						// 	}
 						// }
 					}
 				}
 			} catch (error) {
-				console.log(`${color.cyan}[dlskins] ${color.white}${god.Name} ${color.red}${error}${color.stop}`)
+				console.log(`${color.cya}[dlskins] ${color.whi}${god.Name} ${color.red}${error}${color.stop}`)
 			}
 		}
 	} catch (error) {
 		console.log(error);
 	}
 }
-dlskins().then(() => console.log(`${color.cyan}[dlskins] ${color.green}is over.${color.stop}`));
+dlskins().then(() => console.log(`${color.cya}[dlskins] ${color.grn}is over.${color.stop}`));
