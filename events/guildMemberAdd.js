13cc680df94ffc5ae7e6cb77ab2f7232937e0566
Benjamin Gaertner <benjaming@outlook.fr>
2023-10-13 23:31:41 +0200

    gives studrole on joining

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,5 +7,9 @@ module.exports = {
 		const targetChannel = member.guild.channels.cache.find((channel) => channel.id === '1118450787450892311');
 
 		targetChannel.send(`## Bienvenue ${member}, :wave:\n**Va parler à [Merlin](https://discord.com/channels/1117863871827431434/1121929529003364452/1122621252151033890) dès que possible**,\ntu pourras ensuite mentionner tout les rôles de jeux pour savoir qui veux jouer.`);
+
+		const studRoleId = "1122112439849328651";
+		const studRole = member.guild.cache.get(studRoleId);
+		member.roles.add(studRole).then(console.log(`${clr.cya}[+mbr]	${clr.grn}${user.username} joined.${clr.stop}`))
 	},
 };
