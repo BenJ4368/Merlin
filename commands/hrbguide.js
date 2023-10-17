c88fc33737e14d7ece05d732d2bd39f1ebd9604c
Benjamin Gaertner <benjaming@outlook.fr>
2023-10-18 01:46:06 +0200

    test hrbguide

--- commands/hrbguide.js
+++ commands/hrbguide.js
@@ -17,20 +17,53 @@ module.exports = {
 		console.log(`${clr.cya}[cmd]	${clr.mag}/hrbguide ${clr.whi}was fired by ${clr.blu}${interaction.user.username}${clr.stop}`);
 		if (config.adminIds.includes(interaction.user.id)) {	// Checks if the user triggering the command is one of the admins (admin Ids are stored in the config.js file, in an array)
 
-		const dir = './hrbguide';
-
-		if (fs.existsSync(dir)) {
-			const files = fs.readdirSync(dir);
-			const imageFiles = files.filter(file => file.endsWith(".jpg") || file.endsWith(".png") || file.endsWith(".jpeg"));
-			await Promise.all(imageFiles.map(async (file) => {
-				await sleep(2);
-				interaction.channel.send({ files: [`${dir}/${file}`] });
-			}));
-			await interaction.reply({ content: "Done", ephemeral: true });
-		} else {
-			await interaction.reply("Le dossier spécifié n'existe pas.");
-		}
-
+			await interaction.channel.send({ files: [`./hrbguide/1`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/2`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/3`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/4`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/5`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/6`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/7`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/8`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/9`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/a`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/b`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/c`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/d`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/e`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/f`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/g`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/h`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/i`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/j`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/k`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/l`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/m`] });
+			await sleep(1);
+			await interaction.channel.send({ files: [`./hrbguide/n`] });
+			await sleep(1);
+			interaction.channel.send({ files: [`./hrbguide/o`] });
 			
 		}
 		else	// If the user triggering the command is not an admin, sends an ephemeral message to explain it to him, and console log the denied acces.
