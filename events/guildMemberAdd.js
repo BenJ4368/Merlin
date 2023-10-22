c30a5d4af8e9ecf553fe1809fecaa4f81ccc6ab8
Benjamin Gaertner <benjaming@outlook.fr>
2023-10-22 17:30:27 +0200

    changed welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,7 +4,7 @@ const clr = require("../resources/color_codes");
 module.exports = {
 	name: 'guildMemberAdd',
 	execute(member) {
-		const targetChannel = member.guild.channels.cache.find(channel => channel.id === '1122112439849328651');
+		const targetChannel = member.guild.channels.cache.find(channel => channel.id === '1118450787450892311');
 
 		if (targetChannel) {
 			targetChannel.send(`## Bienvenue ${member}, :wave:\n**[Viens me voir](https://discord.com/channels/1117863871827431434/1121929529003364452/1122621252151033890) dès que possible**.\n N'hésite pas ensuite à mentionner les rôles de jeux, poser des questions, organiser des events, lancer des sorts, combattre des momies, escalader la Tour Eiffel ou laver un singe à l'eau de vaisselle...`);
