05be918f8e8cb404be7e655f802d3a029d76e744
Benjamin Gaertner <benjaming@outlook.fr>
2023-10-22 18:22:40 +0200

    changed welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -10,7 +10,7 @@ module.exports = {
 
 			targetChannel.send(`## Bienvenue ${member}, :wave:\n**[Viens me voir](https://discord.com/channels/1117863871827431434/1121929529003364452/1122621252151033890) dès que possible**.\n N'hésite pas ensuite à mentionner les rôles de jeux, poser des questions, organiser des events, lancer des sorts,\ncombattre des momies, escalader la Tour Eiffel ou laver un singe à l'eau de vaisselle...`);
 
-			const studRoleId = "1162504451953791087";
+			const studRoleId = "1122112439849328651";
 			const studRole = member.guild.roles.cache.get(studRoleId);
 			member.roles.add(studRole).then(console.log(`${clr.cya}[+mbr]	${clr.grn}${member.displayName} joined.${clr.stop}`));
 
