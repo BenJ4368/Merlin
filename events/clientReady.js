2338f5de7199ab24c38a898e4ad4dbfb3d16bb12
Benjamin Gaertner <benjaming@outlook.fr>
2024-01-21 13:22:06 +0100

    update

--- events/clientReady.js
+++ events/clientReady.js
@@ -10,9 +10,9 @@ module.exports = {
 			status: 'online',
 		})
 		bot.user.setActivity({	// Sets the activity
-			name: "BenJ's top 50",	// Name of the game/stream/music etc
+			name: "Smite2 trailer",	// Name of the game/stream/music etc
 			type: Discord.ActivityType.Streaming,	// Type of activity (streaming, playing, listening to, competing etc)
-			url: "https://www.youtube.com/watch?v=cnzfaETtVkU&list=PLUXvz7hjf_iGViHLHoveZwyz5w48rEE-6"	// 'url' property only works with streaming type
+			url: "https://www.youtube.com/watch?v=o1PHxmPq5o4"	// 'url' property only works with streaming type
 		})
 
 		const guildsNbr = bot.guilds.cache.size;  // Stores the numbers of server the bot is in
