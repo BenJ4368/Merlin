4a1269195597e83f6ce2241f42d7cd83e37414d6
Benjamin Gaertner <benjaming@outlook.fr>
2024-11-13 23:57:19 +0100

    vip DEVZONE command

--- commands/vip.js
+++ commands/vip.js
@@ -39,7 +39,7 @@ module.exports = {
 		const member = interaction.guild.members.cache.get(user.id);
 		const role = interaction.guild.roles.cache.find(role => role.name === 'VIP');
 		const currentTime = Date.now();
-		let expirationTime = vipUsers[user.id]?.expiration || currentTime;
+		let expirationTime = vipUsers[user.id]?.expirationTime || currentTime;
 
 		if (!role) return interaction.reply({ content: "Une erreur s'est produite: Le rôle VIP n'existe pas.", ephemeral: true });
 
@@ -51,11 +51,15 @@ module.exports = {
 		}
 
 		if (action === 'add') {
-			expirationTime += duration * 60 * 1000; // milliseconds
-			vipUsers[user.id] = { expiration };
+			expirationTime += duration * 24 * 60 * 60 * 1000; // days -> milliseconds
+			vipUsers[user.id] = { expirationTime };
 
 			if (!member.roles.cache.has(role.id)) {
+				await user.send(`Vous êtes désormais VIP pour une durée de ${duration} jours.`);
 				await member.roles.add(role);
+			}
+			else {
+				await user.send(`Votre statut VIP a été prolongé de ${duration} jours.`);
 			}
 
 			// schedule a job to remove the role when the expiration time is reached
@@ -63,12 +67,13 @@ module.exports = {
 				delete vipUsers[user.id];
 				if (member.roles.cache.has(role.id)) {
 					await member.roles.remove(role);
+					await user.send(`Votre statut VIP a expirer.`);
 					console.log(`${clr.red}[VIP]	${clr.red}${user.username} ${clr.whi}a perdu son statut VIP.`);
 				}
 			});
 
-			// schedule a job to notify the user 5 days before expiration
-			const notifyTime = expirationTime - 5 * 60 * 1000; // 5 days before expiration
+			// schedule a job to notify the user 3 days before expiration
+			const notifyTime = expirationTime - 3 * 24 * 60 * 60 * 1000; // 5 days before expiration
 			schedule.scheduleJob(new Date(notifyTime), async () => {
 				if (vipUsers[user.id]) {
 					await user.send(`Votre statut VIP va expirer dans 5 jours.`);
@@ -80,18 +85,20 @@ module.exports = {
 			interaction.reply({ content: `L'utilisateur ${user.username} a été ajouté à la liste des VIP pour une durée de ${duration} jours.`, ephemeral: true });
 		}
 		else if (action === 'remove') {
-			if (expirationTime - currentTime <= jours * 24 * 60 * 60 * 1000) {
+			if (expirationTime - currentTime <= duration * 24 * 60 * 60 * 1000) {
 				// if time left is less than the duration, remove the role
 				delete vipUsers[user.id];
 				if (member.roles.cache.has(role.id)) {
 					await member.roles.remove(role);
 				}
+				await user.send(`Votre statut VIP vous a été retiré par un administrateur.`);
 				interaction.reply({ content: `L'utilisateur ${user.username} a été retiré de la liste des VIP.`, ephemeral: true });
 			}
 			else {
 				expirationTime -= duration * 24 * 60 * 60 * 1000;
-				vipUsers[user.id].expiration = expiration;
+				vipUsers[user.id].expirationTime = expirationTime;
 				timeLeft = Math.ceil((expirationTime - currentTime) / (24 * 60 * 60 * 1000));
+				await user.send(`La durée de votre statut VIP a été réduite de ${duration} jours. Il vous en reste ${timeLeft} jours.`);
 				interaction.reply({ content: `La durée du statut VIP de ${user.username} a été réduite de ${duration} jours. (reste: ${timeLeft})`, ephemeral: true });
 			}
 		}
@@ -99,16 +106,8 @@ module.exports = {
 			if (interaction.user.id !== user.id) { // if user is trying to check another user's VIP status
 				return interaction.reply({ content: "Vous ne pouvez pas vérifier le detail du statut VIP d'un autre utilisateur.", ephemeral: true });
 			}
-			timeLeft = Math.ceil((expirationTime - currentTime) / (60 * 1000));
+			timeLeft = Math.ceil((expirationTime - currentTime) / (24 * 60 * 60 * 1000));
 			interaction.reply({ content: `Il vous reste ${timeLeft} jours de status VIP.`, ephemeral: true });
-		}
-		else if (action === 'list') {
-			let vipList = 'Voici les utilisateur VIP actuels:\n';
-			for (const id in vipUsers) {
-				const vipUser = interaction.guild.members.cache.get(id);
-				vipList += `${vipUser.user.username}\n`;
-			}
-			interaction.reply({ content: vipList, ephemeral: true });
 		}
 	}
 }
