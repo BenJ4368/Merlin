5bb3c10a73e05b08cc103325c72fa5f87d999152
Benjamin Gaertner <benjaming@outlook.fr>
2024-11-23 13:43:54 +0100

    tweaks

--- database.js
+++ database.js
@@ -13,18 +13,18 @@ const dbClient = new Client({
 dbClient.connect()
     .then(async () => {
 		console.log(`${clr.cya}[PG]	${clr.grn}Connexion à PostgreSQL (${config.DBname}) établie${clr.stop}`);
-		try {
-			await dbClient.query(`
-				CREATE TABLE IF NOT EXISTS vip_users (
-					user_id VARCHAR(50) PRIMARY KEY,
-					expiration_time BIGINT NOT NULL,
-					notified BOOLEAN DEFAULT FALSE
-				);
-			`);
-			console.log(`${clr.cya}[PG]	${clr.grn}OK ${clr.blu}TABLE vip_users${clr.stop}`);
-		} catch (err) {
-			console.error(`${clr.red}[PG]	Erreur lors de la création de la table vip_users${clr.stop}`, err);
-		}
+		// try {
+		// 	await dbClient.query(`
+		// 		CREATE TABLE IF NOT EXISTS vip_users (
+		// 			user_id VARCHAR(50) PRIMARY KEY,
+		// 			expiration_time BIGINT NOT NULL,
+		// 			notified BOOLEAN DEFAULT FALSE
+		// 		);
+		// 	`);
+		// 	console.log(`${clr.cya}[PG]	${clr.grn}OK ${clr.blu}TABLE vip_users${clr.stop}`);
+		// } catch (err) {
+		// 	console.error(`${clr.red}[PG]	Erreur lors de la création de la table vip_users${clr.stop}`, err);
+		// }
 	})
 	.catch(err => console.error(`${clr.red}[PG]	Erreur de connexion à PostgreSQL${clr.stop}`, err));
 
