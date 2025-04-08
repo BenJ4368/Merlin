be88add0b10d4d11850c15c77f0d3027e546c079
Benjamin Gaertner <benjaming@outlook.fr>
2025-04-08 17:19:59 +0200

    my bad

--- events/clientReady.js
+++ events/clientReady.js
@@ -14,13 +14,5 @@ module.exports = {
 			type: Discord.ActivityType.Playing,	// Type of activity (streaming, playing, listening to, competing etc)
 			//url: ""	// 'url' property only works with streaming type
 		})
-
-		const guildsNbr = bot.guilds.cache.size;  // Stores the numbers of server the bot is in
-		console.log(`${clr.cya}[Ready]	${clr.red}${bot.user.username}${clr.grn} is ONLINE. Present in ${guildsNbr} servers${clr.stop}`);
-
-		/* fetching the cinemaRole message's cache for the listeners (cinemaRoleAdd and cinemaRoleReactRemove) */
-		const cinemaRoleChannel = await bot.channels.fetch("1286659163467944017");
-		const cinemaRoleMessage = await cinemaRoleChannel.messages.fetch("1286674813724921969");
-
 	},
 };
